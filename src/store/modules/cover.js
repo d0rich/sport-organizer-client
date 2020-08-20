ab4c738990877b3952dcbf1f6f136eec24be6e96
d0rich <d.orich@yandex.ru>
2020-08-20 14:09:02 +0700

    123

--- src/store/modules/cover.js
+++ src/store/modules/cover.js
@@ -2,6 +2,7 @@ export default {
   state: {
     side_panel_links: [
       { name: 'Profile', text: 'Профиль', icon: 'mdi-account', loggedIn: true, trainer: null, badge: false, badgeKey: 'Login' },
+      { name: 'News', text: 'Мои новости', icon: 'mdi-newspaper-variant', loggedIn: true, trainer: null, badge: false, badgeKey: 'Sections' },
       { name: 'Sections', text: 'Мои секции', icon: 'mdi-ballot', loggedIn: true, trainer: true, badge: true, badgeKey: 'Sections' },
       { name: 'Groups', text: 'Мои группы', icon: 'mdi-account-group', loggedIn: true, trainer: true, badge: true, badgeKey: 'TrainerIn' },
       { name: 'Groups', text: 'Мои группы', icon: 'mdi-account-group', loggedIn: true, trainer: false, badge: true, badgeKey: 'TraineeIn' },
